c7240bc4d0e170d9175986ef56b69f4fffb86414
syed-ahmad-arshad <bsef16m009@pucit.edu.pk>
2021-08-05 10:20:57 +0500

    Changes added.

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from core import models
+from django.utils.translation import gettext as _
+class UserAdmin(BaseUserAdmin):
+    ordering = ['id']
+    list_display = ['email', 'name']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal Info'), {'fields': ('name',)}),
+        (
+            _('Permissions'),
+            {'fields': ('is_active', 'is_staff', 'is_superuser')}
+        ),
+        (_('Important dates'), {'fields': ('last_login',)})
+    )
 
-# Register your models here.
+
+admin.site.register(models.User, UserAdmin)
---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,36 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase, Client
+from django.urls import reverse
+
+
+class AdminSiteTests(TestCase):
+    def setUp(self):
+        self.client=Client()
+        self.admin_user = get_user_model().objects.create_super_user(    
+            email = 'admin@gmail.com',
+            password = 'admin123'
+        )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email = 'test@gmail.com',
+            password = 'test123',
+            name = 'Test user full name.'
+        )
+
+
+    def test_users_listed(self):
+        """ Test that users are listed """
+        url = reverse('admin:core_user_changelist')
+        res = self.client.get(url)
+
+            
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.name)
+
+    def test_user_change_page(self):
+        """ Test that user edit page works """
+        url = reverse('admin:core_user_change', args = [self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+        
