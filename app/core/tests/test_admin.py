e93efe06b860befe42751eb22cc373fce10eef54
syed-ahmad-arshad <bsef16m009@pucit.edu.pk>
2021-08-06 10:01:24 +0500

    Configured postgres DB

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -25,7 +25,7 @@ class AdminSiteTests(TestCase):
 
             
         self.assertContains(res, self.user.name)
-        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
 
     def test_user_change_page(self):
         """ Test that user edit page works """
@@ -33,4 +33,10 @@ class AdminSiteTests(TestCase):
         res = self.client.get(url)
 
         self.assertEqual(res.status_code, 200)
-        
+    
+    def test_create_user_page(self):
+        """ Test that the create user page works """
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
